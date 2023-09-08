0eb971f6e83dbf667d9b0e519433dff39f2b7bb6
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-09-08 16:38:43 -0500

    completed and tested method for deleteAdById in Ads.java and MySQLAdsDao.java

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -9,6 +9,7 @@ public interface Ads {
     List<Ad> all();
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
-
     Ad findAdById(long id);
+
+    void deleteById(long id);
 }
