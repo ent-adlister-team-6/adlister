87419ed46c6601a38e06c58a6ccde2fa31cefa4e
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-09-08 14:25:57 -0500

    missing brack on admin servlet

--- src/main/java/com/codeup/adlister/controllers/AdminPageServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdminPageServlet.java
@@ -13,8 +13,8 @@ import java.io.PrintWriter;
 public class AdminPageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-request.getRequestDispatcher("WEB-INF/adminpage.jsp").forward(request,response);
+        request.getRequestDispatcher("WEB-INF/adminpage.jsp").forward(request, response);
         // Set the content type and character encoding for the response
 
-    
+    }
 }
