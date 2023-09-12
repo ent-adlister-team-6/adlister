5146232f724d2e4cffc27e8309426e8ea51600db
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-09-12 15:53:49 -0500

    WIP adsorting on front end with js

--- src/main/webapp/WEB-INF/JS/ad-sort.js
+++ src/main/webapp/WEB-INF/JS/ad-sort.js
@@ -5,7 +5,7 @@ let sortByTitleBtn = document.querySelector("#sort-title");
 let sortByPriceBtn = document.querySelector("#sort-price");
 let allAdTitles = document.querySelectorAll(".ad-link");
 
-let allAdTitles = document.querySelectorAll(".ad-link");
+let allAdPrices = document.querySelectorAll(".ad-price");
 let adArray = [];
 
 //FUNCTIONS
